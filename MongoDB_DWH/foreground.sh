3780aae61263cd662df2324fc88f3d24aab2c335
Jonathan Brose <jonathanbrose@protonmail.com>
2022-04-16 14:44:43 +0200

    pls work

--- MongoDB_DWH/foreground.sh
+++ MongoDB_DWH/foreground.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 docker-compose up -d
-echo "inserting data into database...."
+apt-get update
+apt-get install python3.8-venv -y
+python -m venv ./venv
+source ./venv/bin/acivate
 pip install -r ./requirements.txt
+echo "inserting data into database...."
 python ./fill_db.py
