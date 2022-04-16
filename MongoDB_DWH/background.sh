97209658d271439fa7b2203916b90a8375f10f7b
Jonathan Brose <jonathanbrose@protonmail.com>
2022-04-16 15:02:08 +0200

    pls work

---
+++ MongoDB_DWH/background.sh
@@ -0,0 +1,2 @@
+apt-get update
+apt-get install python3.8-venv -y
--- MongoDB_DWH/foreground.sh
+++ MongoDB_DWH/foreground.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 docker-compose up -d
-apt-get update
-apt-get install python3.8-venv -y
 python3 -m venv ./venv
 source ./venv/bin/activate
 pip install -r ./requirements.txt
