13c2f0e4f422968cbab7325eb8a807897c9f7700
Jonathan Brose <jonathanbrose@protonmail.com>
2022-04-19 08:04:46 +0200

    pls work...

--- MongoDB_DWH/foreground.sh
+++ MongoDB_DWH/foreground.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 docker-compose up -d
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install -r ./requirements.txt
 source ./venv/bin/activate
 python ./fill_db.py
 echo "you can now move on."
