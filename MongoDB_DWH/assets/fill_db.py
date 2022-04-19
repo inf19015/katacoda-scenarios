7c79620d0240093eea75b227c73d35d587040b4e
Jonathan Brose <jonathanbrose@protonmail.com>
2022-04-19 16:40:50 +0200

    added orders to person sample data

--- MongoDB_DWH/assets/fill_db.py
+++ MongoDB_DWH/assets/fill_db.py
@@ -5,11 +5,18 @@ import pymongo
 def get_database():
     connection_string = "mongodb://localhost"
     client = pymongo.MongoClient(connection_string)
-    return client["test"]
+    return client["dwh"]
 
 
 if __name__ == "__main__":
-    data_persons = json.load(open("data_persons_merged.json", "r"))
     dbname = get_database()
+
+    data_persons = json.load(open("data_persons_merged.json", "r"))
     collection_name = dbname["person"]
     collection_name.insert_many(data_persons)
+
+    data_customers = json.load(open("data_customers_merged.json", "r"))
+    collection_name = dbname["customer"]
+    collection_name.insert_many(data_customers)
+
+
--- MongoDB_DWH/assets/merge_data.py
+++ MongoDB_DWH/assets/merge_data.py
@@ -1,5 +1,6 @@
 import json
 import random
+import uuid
 from datetime import datetime
 
 
@@ -8,6 +9,18 @@ def merge_persons_schools_hobbies(filename):
     possible_education_offsets = 6 * [0] + 3 * [1] + 2 * [2] + [3]
     possible_education_counts = [0] + 4 * [1] + 10 * [2] + 4 * [3] + [4]
     possible_hobbies_counts = [0] + 3 * [1] + 5 * [2] + 3 * [3] + [4]
+    possible_item_counts = [0] + 3 * [1] + 5 * [2] + 3 * [3] + [4]
+    possible_order_counts = [0] + 2*[1] + 2 * [2] + 5 * [3] + 2 * [4] + [i for i in range(5, 30)]
+
+    data_orders = json.load(open("data_orders.json", "r")) * 10
+    data_items = json.load(open("data_items.json", "r"))
+    for order in data_orders:
+        items = []
+        order["order-id"] = str(uuid.uuid4().fields[-1])[:6]
+        for j in range(random.choice(possible_item_counts)):
+            items.append(random.choice(data_items))
+        order["items"] = items
+
     data_persons = json.load(open("data_persons.json", "r"))
     data_schools = json.load(open("data_schools.json", "r"))
     data_hobbies = json.load(open("data_hobbies.json", "r"))
@@ -33,6 +46,13 @@ def merge_persons_schools_hobbies(filename):
             index = random.randint(0, len(possible_hobbies)-1)
             hobbies.append(possible_hobbies.pop(index))
         person["Hobbies"] = hobbies
+
+        possible_orders: list = data_orders.copy()
+        orders = []
+        for i in range(random.choice(possible_order_counts)):
+            index = random.randint(0, len(possible_orders)-1)
+            orders.append(possible_orders.pop(index))
+        person["Orders"] = orders
 
     json.dump(data_persons, open(filename, "w"), indent=4)
 
